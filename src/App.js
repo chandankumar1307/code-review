32bea93f28e29948137600ceb70ae3831ad9733c
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-06-21 22:44:21 +0530

    half part done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import Home from './components/Home'
+import NavBar from './components/NavBar'
+import Stats from './components/Stats'
+
 const App = () => {
   return (
-   <Routes>
- <Route exact path="/Home" element={<Home />}></Route>
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/navbar" element={<NavBar />}></Route>
+      <Route exact path="/header" element={<Header />}></Route>
+      <Route exact path="/stats" element={<Stats />}></Route>
 
-   </Routes>
+    </Routes>
   )
 }
 
